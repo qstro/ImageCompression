25be6d9f0c3ff4cdf27190c3cb846ec7022a464c
qstro <69501446+qstro@users.noreply.github.com>
2022-11-09 14:56:59 +0100

    Update Bildkompression.java

--- Bildkompression.java
+++ Bildkompression.java
@@ -1,3 +1,9 @@
+//Copyright (c) 2012-2022, Quirin Strotzer
+//All rights reserved.
+
+//This source code is licensed under the AGPL-3.0 license license found in the
+//LICENSE file in the root directory of this source tree. 
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
